758ebeafb4bfb2f1576aa9afbaf6557da0c3a7f2
MarthaGM91 <marthagm91@gmail.com>
2024-08-22 17:04:34 -0400

    Comenzando a usar Grid, el plan es evolucionar hasta llegar al estilo Bento

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 /* envolver tu código JavaScript en un evento */
-document.addEventListener("DOMContentLoaded", function(){
-   /* que funcion tiene esto */
-    const themeToggleButton = document.getElementById('theme-toggle'); 
+document.addEventListener("DOMContentLoaded", function () {
+    /* que funcion tiene esto */
+    const themeToggleButton = document.getElementById('theme-toggle');
 
-    themeToggleButton.addEventListener('click',()=> {
+    themeToggleButton.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
-    
+
+        // Cambiar texto e icono según el modo
+        if (document.body.classList.contains('dark-mode')) {
+            this.querySelector('.text').textContent = 'Light Mode';
+            this.querySelector('.icon').textContent = '☀️';
+        } else {
+            this.querySelector('.text').textContent = 'Dark Mode';
+            this.querySelector('.icon').textContent = '🌙';
+        }
     });
 
 });
