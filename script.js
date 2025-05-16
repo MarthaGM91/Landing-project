425871124f591fc3603814d3b7a2b503c4b23a16
Martha Garcia <marthagm91@gmail.com>
2025-05-16 16:44:01 -0400

    actualizacion de varios archivos

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-/* envolver tu código JavaScript en un evento */
-document.addEventListener("DOMContentLoaded", function () {
-    /* que funcion tiene esto */
-    const themeToggleButton = document.getElementById('theme-toggle');
+// /* envolver tu código JavaScript en un evento */
+// document.addEventListener("DOMContentLoaded", function () {
+//     /* que funcion tiene esto */
+//     const themeToggleButton = document.getElementById('theme-toggle');
 
-    themeToggleButton.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
+//     themeToggleButton.addEventListener('click', () => {
+//         document.body.classList.toggle('dark-mode');
 
-        // Cambiar texto e icono según el modo
-        if (document.body.classList.contains('dark-mode')) {
-            this.querySelector('.text').textContent = 'Light Mode';
-            this.querySelector('.icon').textContent = '☀️';
-        } else {
-            this.querySelector('.text').textContent = 'Dark Mode';
-            this.querySelector('.icon').textContent = '🌙';
-        }
+//         // Cambiar texto e icono según el modo
+//         if (document.body.classList.contains('dark-mode')) {
+//             this.querySelector('.text').textContent = 'Light Mode';
+//             this.querySelector('.icon').textContent = '☀️';
+//         } else {
+//             this.querySelector('.text').textContent = 'Dark Mode';
+//             this.querySelector('.icon').textContent = '🌙';
+//         }
 
         
-    });
+//     });
 
-});
+// });
